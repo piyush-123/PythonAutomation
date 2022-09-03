aae2a3c9edb4dcc66d90c7b9e5696272fb97f759
Piyush Aggarwal <piyushricky1988@gmail.com>
2022-09-03 14:06:04 +0530

    version change in runtime d123445678900197

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("window-size=1400,900")
 
 
 @app.route('/',methods=['GET'])
